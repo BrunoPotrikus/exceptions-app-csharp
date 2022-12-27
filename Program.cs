ce8770950f743ca37193d203df098b93d9ff06d1
BrunoPotrikus <brunopotrikus@gmail.com>
2022-12-27 00:48:52 -0300

    Disparando erros com throw

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ public class Program
 
         try
         {
-            Console.Clear();
-            for (var index = 0; index < 10; index++)
-            {
-                Console.WriteLine(arr[index]);
-            }
+            //Console.Clear();
+            //for (var index = 0; index < 10; index++)
+            //{
+            //    Console.WriteLine(arr[index]);
+            //}
+
+            Register("");
         }
         catch(IndexOutOfRangeException exp)
         {
@@ -20,12 +22,25 @@ public class Program
             Console.WriteLine(exp.Message);
             Console.WriteLine("Índice não encontrado!");
         }
+        catch(ArgumentNullException exp)
+        {
+            Console.WriteLine(exp.InnerException);
+            Console.WriteLine(exp.Message);
+            Console.WriteLine("Falha ao cadastrar o texto!");
+        }
         catch (Exception exp)
         {
             Console.Clear();
             Console.WriteLine(exp.InnerException);
             Console.WriteLine(exp.Message);
             Console.WriteLine("Desculpe, algo deu errado!!!");
+        }
+    }
+    private static void Register(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
         }
     }
 }
