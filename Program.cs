f0d2c169a08af738d73dabc59b966a8c42da17bb
BrunoPotrikus <brunopotrikus@gmail.com>
2022-12-27 00:27:19 -0300

    Primeiro commit, com tratamento de erros usando try/catch

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var arr = new int[5];
+
+        try
+        {
+            Console.Clear();
+            for (var index = 0; index < 10; index++)
+            {
+                Console.WriteLine(arr[index]);
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.Clear();
+            Console.WriteLine(exp.InnerException);
+            Console.WriteLine(exp.Message);
+            Console.WriteLine("Desculpe, algo deu errado!!!");
+        }
+    }
+}
